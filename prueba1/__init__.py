9c1a6f492386e0c7bdda683fa3d8657b5f63cef0
AGMadera <ponchp.agm@gmail.com>
2017-03-25 23:27:32 -0600

    Initial commit

---
+++ prueba1/__init__.py
@@ -0,0 +1,22 @@
+print "hola el poncho la rifa"
+
+a = "es la varibale 1"
+
+#lista es como un array
+lista = [1,2,3,"caca"]
+print (list(lista))
+
+lista2 = "Hola mijares"
+print(list(lista2))
+
+#Tupla como array inmutable
+tuplatumadre=(1,"la unidad la 1",2, a)
+print(tuple(tuplatumadre))
+
+#Diccionario es un Json
+diccionario = {"agresion":"vas y ching... u madre", "valor2":2, "valorN":777}
+
+print(diccionario)
+
+diccionario2 = {"agresion":"gatu adre pend...", "valor2":4, "valorN":777}
+
